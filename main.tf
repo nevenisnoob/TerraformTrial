be4169ad9805804f9abaf9555e18bb87f03d5619
Liangjin Huang <liangjin.huang@dena.jp>
2023-01-11 18:18:33 +0900

    add vpc2

--- main.tf
+++ main.tf
@@ -39,6 +39,12 @@ resource "google_compute_instance" "default" {
   }
 }
 
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-custom-mode-network"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
+
 resource "google_compute_subnetwork" "default" {
   name          = "my-custom-subnet"
   ip_cidr_range = "10.0.1.0/24"
