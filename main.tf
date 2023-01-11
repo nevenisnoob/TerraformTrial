fff46766139bf034091c73c5dc4f6b0ebb71bdbb
Liangjin Huang <liangjin.huang@dena.jp>
2023-01-11 18:13:54 +0900

    add compute engine

--- main.tf
+++ main.tf
@@ -12,3 +12,29 @@ terraform {
     prefix = "state" # any prefix is ok.
   }
 }
+
+
+
+resource "google_compute_instance" "default" {
+  name         = "flask-vm2"
+  machine_type = "f1-micro"
+  zone         = "us-west1-a"
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  # Install Flask
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install flask"
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.default.id
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
+}
