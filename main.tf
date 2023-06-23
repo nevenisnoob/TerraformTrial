0e2392513bae4c8e87096645a8674f30d90446e5
Liangjin Huang <liangjin.huang@dena.jp>
2023-06-23 18:02:47 +0900

    gcs alert

--- main.tf
+++ main.tf
@@ -14,39 +14,64 @@ terraform {
 }
 
 
-resource "google_compute_instance" "default" {
-  name         = "flask-vm2"
-  machine_type = "f1-micro"
-  zone         = "us-west1-a"
-  tags         = ["ssh"]
+resource "google_project_iam_custom_role" "terraform-ci" {
+  title       = "TerraformCI"
+  role_id     = "Terraform CI Custom Role"
+  description = "TerraformCIのCustomRole"
+  stage       = "ALPHA"
+  permissions = [
+    "storage.objects.list"
+  ]
+}
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
+resource "google_project_iam_member" "terraform-role" {
+  project = local.project_id
+  role    = google_project_iam_custom_role.terraform-ci.id
+  member  = "serviceAccount:${google_service_account.workload_identity_federation.email}"
+}
+
+
+resource "google_project_iam_audit_config" "enable_gcs_logging" {
+  project = var.project_id
+  service = "storage.googleapis.com"
+  audit_log_config {
+    log_type = "DATA_READ"
   }
+  audit_log_config {
+    log_type = "DATA_WRITE"
+  }
+}
 
-  # Install Flask
-  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install flask"
+resource "google_monitoring_alert_policy" "alert_gcs_with_call_ip" {
+  display_name = "GCS Caller IP Alert"
+  combiner     = "OR"
+  conditions {
+    condition_matched_log {
+      filter = join("", [
+        "resource.type=gcs_bucket AND ",
+        "(protoPayload.authorizationInfo.permission = \"storage.objects.get\" OR ",
+        "protoPayload.authorizationInfo.permission = \"storage.objects.update\" OR ",
+        "protoPayload.authorizationInfo.permission = \"storage.objects.create\" OR ",
+        "protoPayload.authorizationInfo.permission = \"storage.objects.delete\") AND ",
+        "NOT (",
+        "${join(" OR ", formatlist("ip_in_net(protoPayload.requestMetadata.callerIp, \"%s\")", var.valid_caller_ips))}",
+        ")",
+      ])
+    }
+    display_name = "GCS Caller IP Alert"
+  }
+  enabled               = true
+  project               = local.project_id
+  notification_channels = [data.google_monitoring_notification_channel.gcs_unknown_access_notify_channel.id]
+  alert_strategy {
+    auto_close = "604800s" #Incident autoclose duration: 7d
 
-  network_interface {
-    subnetwork = google_compute_subnetwork.default.id
-
-    access_config {
-      # Include this section to give the VM an external IP address
+    notification_rate_limit {
+      period = "86400s" #  One notification per 1 day
     }
   }
 }
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "my-custom-mode-network"
-  auto_create_subnetworks = false
-  mtu                     = 1460
-}
-
-resource "google_compute_subnetwork" "default" {
-  name          = "my-custom-subnet"
-  ip_cidr_range = "10.0.1.0/24"
-  region        = "us-west1"
-  network       = google_compute_network.vpc_network.id
+data "google_monitoring_notification_channel" "gcs_unknown_access_notify_channel" {
+  display_name = "GCP-Alert"
 }
